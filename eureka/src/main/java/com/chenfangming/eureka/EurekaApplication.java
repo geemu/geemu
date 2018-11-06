bc92ea7f563d1b6ade9d51a3d3ee978446d2878c
陈方明 <cfmmail@sina.com>
2018-11-06 22:01:19 +0800

    注册中心

---
+++ eureka/src/main/java/com/chenfangming/eureka/EurekaApplication.java
@@ -0,0 +1,25 @@
+package com.chenfangming.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka程序入口
+ * @author 陈方明  cfmmail@sina.com
+ * @since 2018-11-06 21:02
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    /**
+     * 主函数
+     * @param args 运行参数
+     */
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(EurekaApplication.class);
+        // 关闭启动Banner
+        app.setBannerMode(org.springframework.boot.Banner.Mode.OFF);
+        app.run(args);
+    }
+}
