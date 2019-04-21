b29efbf41f2c1466a287ee40a335128297a8cc0e
陈方明 <cfmmail@sina.com>
2019-04-21 11:29:37 +0800

    init

--- config/src/main/java/com/chenfangming/backend/cloud/config/ConfigApplication.java
+++ config/src/main/java/com/chenfangming/backend/cloud/config/ConfigApplication.java
@@ -3,6 +3,7 @@ package com.chenfangming.backend.cloud.config;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @since 2018-11-06 21:02
  */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigApplication {
     /**
