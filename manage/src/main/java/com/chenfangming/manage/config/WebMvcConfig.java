09dcacf1adcd253476a7f31aa420ad7caf50c1d4
陈方明 <cfmmail@sina.com>
2019-06-10 21:48:20 +0800

    init

---
+++ manage/src/main/java/com/chenfangming/manage/config/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package com.chenfangming.manage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * com.chenfangming.manage.config
+ * @author 陈方明  cfmmail@sina.com
+ * @since 2019-06-10 21:30
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    /**
+     * Add handlers to serve static resources such as images, js, and, css
+     * files from specific locations under web application root, the classpath,
+     * and others.
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        registry.addResourceHandler("/swagger-resources/**")
+                .addResourceLocations("classpath:/META-INF/resources/swagger-resources/");
+
+        registry.addResourceHandler("/swagger/**")
+                .addResourceLocations("classpath:/META-INF/resources/swagger*");
+
+        registry.addResourceHandler("/v2/api-docs/**")
+                .addResourceLocations("classpath:/META-INF/resources/v2/api-docs/");
+    }
+}
