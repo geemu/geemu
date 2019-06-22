ef18377852ce236bd4552dcbfbbbe30ced4b9d22
陈方明 <cfmmail@sina.com>
2019-06-22 17:57:19 +0800

    init

--- manage/src/main/java/com/chenfangming/manage/config/security/UserDetailServiceImpl.java
+++ manage/src/main/java/com/chenfangming/manage/config/security/UserDetailServiceImpl.java
@@ -37,17 +37,19 @@ public class UserDetailServiceImpl implements UserDetailsService {
      */
     @Override
     public UserDetails loadUserByUsername(String userName) {
-        //  查询用户
+        // 查询用户
         UserEntity userEntity = userMapper.selectByName(userName)
                 .orElseThrow(() -> new UsernameNotFoundException("用户名不存在"));
-        //  查询用户所拥有的角色  有效的
+        // 查询用户所拥有的角色  有效的
         List<RoleEntity> roleEntityList = roleMapper.selectByUserId(userEntity.getId());
         List<GrantedAuthority> authorities = new LinkedList<>();
         for (RoleEntity role : roleEntityList) {
-            //  这边应该放角色id
+            // 这边应该放角色id
             authorities.add(new SimpleGrantedAuthority(role.getId().toString()));
         }
-        return new User(userEntity.getName(), userEntity.getPassword(), userEntity.getEnabled(),
+        return new User(userEntity.getName(),
+                userEntity.getPassword(),
+                userEntity.getEnabled(),
                 Boolean.TRUE, Boolean.TRUE, Boolean.TRUE, authorities);
     }
 
--- manage/src/main/java/com/chenfangming/manage/config/security/WebSecurityConfig.java
+++ manage/src/main/java/com/chenfangming/manage/config/security/WebSecurityConfig.java
@@ -55,10 +55,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .requestCache().disable().headers().cacheControl().disable()
+                .requestCache().disable()
+                .headers().cacheControl().disable()
 //                .and().addFilterAt(myUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 //  设置匿名用户为0
-//                .anonymous().authorities("0")
+                .and()
+                .anonymous().authorities("0")
                 .and().cors().disable().csrf().disable().httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 .and().logout().clearAuthentication(Boolean.TRUE).logoutSuccessHandler(handler)
