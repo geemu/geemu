05ec58133bf5d9bd95c2585d47f4fca2702a1755
陈方明 <cfmmail@sina.com>
2019-06-29 12:52:18 +0800

    init

--- manage/src/main/java/com/chenfangming/manage/auth/IGrantedAuthority.java
+++
@@ -1,9 +0,0 @@
-package com.chenfangming.manage.auth;
-
-/**
- * com.chenfangming.manage.auth
- * @author 陈方明  cfmmail@sina.com
- * @since 2019-06-22 21:25
- */
-public interface IGrantedAuthority {
-}
