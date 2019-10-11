828e3eff9e25a68a88e72bfdcb7d639b4ca6d4df
陈方明 <cfmmail@sina.com>
2019-10-11 21:48:21 +0800

    init

--- manage/src/main/java/com/chenfangming/manage/persistence/entity/view/MenuRoleView.java
+++ manage/src/main/java/com/chenfangming/manage/persistence/entity/view/MenuRoleView.java
@@ -3,9 +3,12 @@ package com.chenfangming.manage.persistence.entity.view;
 import com.chenfangming.manage.persistence.entity.MenuEntity;
 import com.chenfangming.manage.persistence.entity.RoleEntity;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.List;
@@ -15,14 +18,17 @@ import java.util.List;
  * @author 陈方明  cfmmail@sina.com
  * @since 2019-06-16 18:52
  */
-@Data
+@Getter
+@Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class MenuRoleView extends MenuEntity implements Serializable {
 
     /** 序列化id **/
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -1L;
 
     /** 资源对应的角色集合 **/
     private List<RoleEntity> roleEntityList;
--- manage/src/main/java/com/chenfangming/manage/persistence/entity/view/UserRoleView.java
+++ manage/src/main/java/com/chenfangming/manage/persistence/entity/view/UserRoleView.java
@@ -3,9 +3,12 @@ package com.chenfangming.manage.persistence.entity.view;
 import com.chenfangming.manage.persistence.entity.RoleEntity;
 import com.chenfangming.manage.persistence.entity.UserEntity;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.List;
@@ -15,14 +18,17 @@ import java.util.List;
  * @author 陈方明  cfmmail@sina.com
  * @since 2019-06-16 21:43
  */
-@Data
+@Getter
+@Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class UserRoleView extends UserEntity implements Serializable {
 
     /** 序列化id **/
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -1L;
 
     /** 用户对应的角色集合 **/
     private List<RoleEntity> roleEntityList;
