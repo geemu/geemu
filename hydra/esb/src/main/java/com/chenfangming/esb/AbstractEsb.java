8e12bcd9a0ab53432ab6029bf99ae18f37baa4fd
陈方明 <cfmmail@sina.com>
2019-10-18 22:05:29 +0800

    init

--- hydra/esb/src/main/java/com/chenfangming/esb/AbstractEsb.java
+++ hydra/esb/src/main/java/com/chenfangming/esb/AbstractEsb.java
@@ -8,4 +8,32 @@ package com.chenfangming.esb;
  */
 public abstract class AbstractEsb<Head extends EsbHeadStyle, Body extends EsbBodyStyle> implements Esb<Head, Body> {
 
+    /**
+     * 获取Head
+     * @return Head
+     */
+    @Override
+    public abstract Head getHead();
+
+    /**
+     * 设置Head
+     * @param head head
+     */
+    @Override
+    public abstract void setHead(Head head);
+
+    /**
+     * 获取Body
+     * @return B
+     */
+    @Override
+    public abstract Body getBody();
+
+    /**
+     * 设置Body
+     * @param body body
+     */
+    @Override
+    public abstract void setBody(Body body);
+
 }
